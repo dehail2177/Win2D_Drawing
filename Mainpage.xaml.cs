326e7389f106e984b4668934b43c8dbfedde4423
dehail2177 <50616134+dehail2177@users.noreply.github.com>
2023-11-12 15:14:59 +0900

    Create Mainpage.xaml.cs

---
+++ Mainpage.xaml.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml.Controls;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private void Canvas_Loaded(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    // The rest of your existing code, including Canvas_Draw method
+}
